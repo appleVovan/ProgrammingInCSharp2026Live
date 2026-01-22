988f54b20818d39477ecdd8ca46170e090e854d7
applevovan <yablonskyi.v.reg@gmail.com>
2026-01-22 13:54:33 +0200

    Add project files.

---
+++ Samples/Dog.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KMA.ProgrammingInChsarp.Samples
+{
+    enum DogType
+    {
+        Shepard,
+        Labrador = 200,
+        Poodle,
+        Labradoodle = 5,
+    }
+    internal class Dog
+    {
+        private int _age;
+        private string _name;
+
+        public int Age 
+        {
+            get
+            {  
+                return _age; 
+            }
+            set
+            {
+                _age = value;
+            }
+        }
+
+        public string Name { get => _name; set => _name = value; }
+        public string Identifier
+        {
+            get
+            {
+                return $"Name: {_name} Age: {_age}";
+            }
+        }
+
+        private DogType _breed;
+
+        public DogType Breed
+        {
+            get { return _breed; }  
+            set { _breed = value; }
+        }
+
+
+        private void Foo()
+        {
+
+        }
+
+        internal void Bar()
+        {
+
+        }
+
+        protected void Gas()
+        {
+
+        }
+
+        public void Talk(string value)
+        {
+
+        }
+
+        public void Talk(int times, string value = "woof", bool sit = false)
+        {
+
+        }
+
+
+        public void MyMethod()
+        {
+            
+            int val1 = Age;
+            Age = 5;
+
+            switch (Breed)
+            {
+                case DogType.Shepard:
+                    break;
+                case DogType.Labrador:
+                    break;
+                case DogType.Poodle:
+                case DogType.Labradoodle:
+                    break;
+            }
+
+            Talk("bark");
+            Talk(3);
+            Talk(3, "bark");
+            Talk(3, sit:true);
+        }
+    }
+}
---
+++ Samples/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace KMA.ProgrammingInChsarp.Samples
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("Hello, World!");
+        }
+    }
+}
