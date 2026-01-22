f131299a331770fb93e525c75efdea3a8eef0f93
applevovan <yablonskyi.v.reg@gmail.com>
2026-01-22 15:07:35 +0200

    Commit example

--- Samples/Program.cs
+++ Samples/Program.cs
@@ -1,10 +1,33 @@
-﻿namespace KMA.ProgrammingInChsarp.Samples
+﻿using KMA.ProgrammingInChsarp2026.Samples.Original;
+using KMA.ProgrammingInChsarp2026.Samples.Copy;
+
+namespace KMA.ProgrammingInChsarp2026.Samples
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            System.Console.WriteLine("Hello, World!");
+            Sample1();
         }
+
+        #region Initialization in C# Examples
+        static void Sample1()
+        {
+            var mystudent1 = new Original.Student() { FirstName = "Steve", LastName = "Jobs" };
+
+            Copy.Student mystudent2 = new Copy.Student();
+            mystudent1.FirstName = "Bill";
+            mystudent1.LastName = "Gates";
+        }
+        #endregion
+
+        #region Variable Initialization Examples
+        static void Sample2()
+        {
+            int i, j, k;
+
+            i = j = k = 0;
+        }
+        #endregion
     }
 }
---
+++ Samples/Student - Copy.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace KMA.ProgrammingInChsarp2026.Samples.Copy
+{
+    internal class Student
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public Student(string firstName = "Default", string lastName = "Default")
+        {
+            FirstName = firstName;
+            LastName = lastName;
+        }
+
+        public string GetFullName()
+        {
+            return FirstName + " " + LastName;
+        }
+    }
+}
