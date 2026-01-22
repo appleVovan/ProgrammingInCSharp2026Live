18005384713bef19927ce673e6ba7ef155d55989
applevovan <yablonskyi.v.reg@gmail.com>
2026-01-22 16:54:22 +0200

    Week2: Session results 3ae2e98

--- Samples/Program.cs
+++ Samples/Program.cs
@@ -1,5 +1,8 @@
 ﻿using KMA.ProgrammingInChsarp2026.Samples.Original;
 using KMA.ProgrammingInChsarp2026.Samples.Copy;
+using циферка = System.Int32;
+using OriginalStudent = KMA.ProgrammingInChsarp2026.Samples.Original.Student;
+using CopyStudent = KMA.ProgrammingInChsarp2026.Samples.Copy.Student;
 
 namespace KMA.ProgrammingInChsarp2026.Samples
 {
@@ -7,15 +10,15 @@ namespace KMA.ProgrammingInChsarp2026.Samples
     {
         static void Main(string[] args)
         {
-            Sample1();
+            Sample6();
         }
 
         #region Initialization in C# Examples
         static void Sample1()
         {
-            var mystudent1 = new Original.Student() { FirstName = "Steve", LastName = "Jobs" };
+            var mystudent1 = new OriginalStudent() { FirstName = "Steve", LastName = "Jobs" };
 
-            Copy.Student mystudent2 = new Copy.Student();
+            CopyStudent mystudent2 = new CopyStudent();
             mystudent1.FirstName = "Bill";
             mystudent1.LastName = "Gates";
         }
@@ -24,9 +27,70 @@ namespace KMA.ProgrammingInChsarp2026.Samples
         #region Variable Initialization Examples
         static void Sample2()
         {
-            int i, j, k;
+            циферка i, j, k;
 
             i = j = k = 0;
+        }
+        #endregion
+
+        #region Type conversion Examples
+        static void Sample3()
+        {
+            int x = 1000;
+
+            byte y = checked((byte)x);
+
+            Console.WriteLine(y);
+        }
+        #endregion
+
+        #region New Line Examples
+        static void Sample4()
+        {
+            string tempVar = "My name is Volodymyr." + Environment.NewLine + "My age is 30.";
+        }
+        #endregion
+
+        #region Value & Reference Type Behaviour Examples
+        static void Sample5()
+        {
+            int myInt;
+            MyMethodInt(out myInt);
+            Console.WriteLine(myInt);
+        }
+
+        static void MyMethodInt(out int myInt)
+        {
+            myInt = 6;
+            Console.WriteLine(myInt);
+        }
+        #endregion
+
+        #region Reference Type Behaviour Examples
+        class MyClass
+        {
+            private MyClass myChild;
+
+            public int MyProperty { get; set; }
+            public MyClass MyChild { get => myChild; set => myChild = value; }
+        }
+
+        static void Sample6()
+        {
+            MyClass myObject = new MyClass();//100
+            myObject.MyProperty = 5;//100
+            myObject.MyChild = new MyClass();//110
+            myObject.MyChild.MyProperty = 5;
+            var myObjectMyChild = myObject.MyChild;//110
+            MyMethodObject(ref myObjectMyChild);//120
+            Console.WriteLine(myObject.MyChild.MyProperty);//110
+        }
+
+        static void MyMethodObject(ref MyClass myObject) //110
+        {
+            myObject = new MyClass();//120
+            myObject.MyProperty = 6;
+            Console.WriteLine(myObject.MyProperty);
         }
         #endregion
     }
