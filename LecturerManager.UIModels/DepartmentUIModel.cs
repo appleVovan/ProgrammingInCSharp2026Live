bf02c150c6883c2f0af7b109410a6feabbcc0e55
applevovan <yablonskyi.v.reg@gmail.com>
2026-02-12 14:44:13 +0200

    Practice1 completed

--- LecturerManager.UIModels/DepartmentUIModel.cs
+++ LecturerManager.UIModels/DepartmentUIModel.cs
@@ -1,5 +1,6 @@
 ﻿using KMA.ProgrammingInChsarp2026.LecturerManager.Common.Enums;
 using KMA.ProgrammingInChsarp2026.LecturerManager.DBModels;
+using KMA.ProgrammingInCSharp2026.LecturerManager.Services;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -13,27 +14,27 @@ namespace KMA.ProgrammingInChsarp2026.LecturerManager.UIModels
         private Faculty _faculty;
         private List<LecturerUIModel> _lecturers;
 
-        public Guid Id 
+        public Guid? Id
         {
-            get => _dbModel.Id; 
+            get => _dbModel?.Id;
         }
-        public string Name 
+        public string Name
         {
             get => _name;
             set => _name = value;
         }
-        public Faculty Faculty 
-        { 
+        public Faculty Faculty
+        {
             get => _faculty;
             set => _faculty = value;
         }
         public IReadOnlyList<LecturerUIModel> Lecturers
-        { 
+        {
             get => _lecturers;
         }
 
-        public int Staff 
-        { 
+        public int Staff
+        {
             get => Lecturers?.Count ?? 0;
         }
 
@@ -47,6 +48,35 @@ namespace KMA.ProgrammingInChsarp2026.LecturerManager.UIModels
             _dbModel = dbModel;
             _name = dbModel.Name;
             _faculty = dbModel.Faculty;
+        }
+
+        public void SaveChangesToDBModel()
+        {
+            if (_dbModel != null)
+            {
+                _dbModel.Name = _name;
+                _dbModel.Faculty = _faculty;
+            }
+            else
+            {
+                _dbModel = new DepartmentDBModel(_name, _faculty);
+            }
+        }
+
+        public void LoadLecturers(StorageService storage)
+        {
+            if (Id == null || _lecturers.Count > 0)
+                return;
+
+            foreach (var lecturerDB in storage.GetLecturers(Id.Value))
+            {
+                _lecturers.Add(new LecturerUIModel(lecturerDB));
+            }
+        }
+
+        public override string ToString()
+        {
+            return $"Department: {Name}, Faculty: {Faculty}, Staff: {Staff}";
         }
     }
 }
--- LecturerManager.UIModels/LecturerUIModel.cs
+++ LecturerManager.UIModels/LecturerUIModel.cs
@@ -1,19 +1,104 @@
 ﻿using KMA.ProgrammingInChsarp2026.LecturerManager.Common.Enums;
+using KMA.ProgrammingInChsarp2026.LecturerManager.DBModels;
 using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Text;
+using System.Xml.Linq;
 
 namespace KMA.ProgrammingInChsarp2026.LecturerManager.UIModels
 {
     public class LecturerUIModel
     {
-        public Guid Id { get; set; }
-        public Guid DepartmentGuid { get; set; }
-        public string FirstName { get; set; }
-        public string LastName { get; set; }
-        public LecturerPosition Position { get; set; }
-        public DateTime DateOfBirth { get; set; }
+        private LecturerDBModel _dbModel;
+        private Guid _departmentId;
+        private string _firstName;
+        private string _lastName;
+        private LecturerPosition _position;
+        private DateTime _dateOfBirth;
+        private int _age;
 
-        public int Age { get; set; }
+        public Guid? Id
+        {
+            get => _dbModel?.Id;
+        }
+        public Guid DepartmentId
+        {
+            get => _departmentId;
+        }
+        public string FirstName
+        {
+            get => _firstName;
+            set => _firstName = value;
+        }
+        public string LastName
+        {
+            get => _lastName;
+            set => _lastName = value;
+        }
+        public LecturerPosition Position
+        {
+            get => _position;
+            set => _position = value;
+        }
+        public DateTime DateOfBirth
+        {
+            get => _dateOfBirth;
+            set
+            {
+                if (_dbModel != null)
+                    return; //Prevent changing DateOfBirth if DBModel exists
+                _dateOfBirth = value;
+                CalculateAge();
+            }
+        }
+        public int Age
+        {
+            get => _age;
+        }
+
+        // Constructor used to create new Lecturer
+        public LecturerUIModel(Guid departmentId)
+        {
+            _departmentId = departmentId;
+        }
+        // Constructor used to load existing Department for viewing/editing
+        public LecturerUIModel(LecturerDBModel dBModel)
+        {
+            _dbModel = dBModel;
+            _departmentId = dBModel.DepartmentId;
+            _firstName = dBModel.FirstName;
+            _lastName = dBModel.LastName;
+            _position = dBModel.Position;
+            _dateOfBirth = dBModel.DateOfBirth;
+            CalculateAge();
+        }
+
+        private void CalculateAge()
+        {
+            var today = DateTime.Today;
+            _age = today.Year - _dateOfBirth.Year;
+            if (_dateOfBirth.Date > today.AddYears(-_age))
+                _age--;
+        }
+
+        public void SaveChangesToDBModel()
+        {
+            if (_dbModel != null)
+            {
+                _dbModel.FirstName = _firstName;
+                _dbModel.LastName = _lastName;
+                _dbModel.Position = _position;
+            }
+            else
+            {
+                _dbModel = new LecturerDBModel(_departmentId, _firstName, _lastName, _position, _dateOfBirth);
+            }
+        }
+
+        public override string ToString()
+        {
+            return $"Lecturer: {FirstName} {LastName}, {Position}, Age: {Age}"; ;
+        }
     }
 }
