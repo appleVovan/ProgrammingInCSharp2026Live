a72990ba010c0d7e362425ce5215d8be4e8b1ff7
applevovan <yablonskyi.v.reg@gmail.com>
2026-02-19 14:36:07 +0200

    Week 6: xaml samples

---
+++ LecturerManager/Pages/LecturerCreatePage.xaml.cs
@@ -0,0 +1,34 @@
+namespace KMA.ProgrammingInCSharp2026.LecturerManager.Pages;
+
+public partial class LecturerCreatePage : ContentPage
+{
+	public LecturerCreatePage()
+	{
+		InitializeComponent();
+	}
+
+    private void Button_Clicked(object sender, EventArgs e)
+    {
+		var currentBut = (Button)sender;
+		currentBut.Text += " Ha";
+		if (currentBut.Text.StartsWith("Button 2"))
+		{
+			var newBut = new Button();
+			newBut.Text = "Button 4";
+			newBut.Clicked += Button_Clicked;
+			Grid grid = (Grid)Content;
+			grid.Children.Add(newBut);
+			grid.SetRow(newBut, 1);
+            grid.SetColumn(newBut, 1);
+        } else if (currentBut.Text.StartsWith("Button 4"))
+		{
+            var newBut = new Button();
+            newBut.Text = "Button 1";
+            newBut.Clicked += Button_Clicked;
+            Grid grid = (Grid)Content;
+            grid.Children.Add(newBut);
+            grid.SetRow(newBut, 0);
+            grid.SetColumn(newBut, 0);
+        }
+    }
+}
