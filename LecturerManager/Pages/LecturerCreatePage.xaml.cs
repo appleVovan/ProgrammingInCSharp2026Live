6972aa82fc241e544693e81f8e4a7c8780f660d2
applevovan <yablonskyi.v.reg@gmail.com>
2026-02-19 15:53:03 +0200

    Week 6: LecturerCreatePage was added and implemented

--- LecturerManager/Pages/LecturerCreatePage.xaml.cs
+++ LecturerManager/Pages/LecturerCreatePage.xaml.cs
@@ -1,3 +1,9 @@
+using KMA.ProgrammingInChsarp2026.LecturerManager.Common;
+using KMA.ProgrammingInChsarp2026.LecturerManager.Common.Enums;
+using KMA.ProgrammingInChsarp2026.LecturerManager.UIModels;
+using System.ComponentModel.DataAnnotations;
+using System.Reflection;
+
 namespace KMA.ProgrammingInCSharp2026.LecturerManager.Pages;
 
 public partial class LecturerCreatePage : ContentPage
@@ -5,30 +11,42 @@ public partial class LecturerCreatePage : ContentPage
 	public LecturerCreatePage()
 	{
 		InitializeComponent();
-	}
+        pPosition.ItemsSource = EnumExtensions.GetValueWithNames<LecturerPosition>();
+    }
 
-    private void Button_Clicked(object sender, EventArgs e)
+    private void CreateClicked(object sender, EventArgs e)
     {
-		var currentBut = (Button)sender;
-		currentBut.Text += " Ha";
-		if (currentBut.Text.StartsWith("Button 2"))
-		{
-			var newBut = new Button();
-			newBut.Text = "Button 4";
-			newBut.Clicked += Button_Clicked;
-			Grid grid = (Grid)Content;
-			grid.Children.Add(newBut);
-			grid.SetRow(newBut, 1);
-            grid.SetColumn(newBut, 1);
-        } else if (currentBut.Text.StartsWith("Button 4"))
-		{
-            var newBut = new Button();
-            newBut.Text = "Button 1";
-            newBut.Clicked += Button_Clicked;
-            Grid grid = (Grid)Content;
-            grid.Children.Add(newBut);
-            grid.SetRow(newBut, 0);
-            grid.SetColumn(newBut, 0);
+        if (String.IsNullOrWhiteSpace(eFistName.Text))
+        {
+            DisplayAlert("Incomplete data!", "First Name can't be empty", "OK");
+            return;
         }
+        if (String.IsNullOrWhiteSpace(eLastName.Text))
+        {
+            DisplayAlert("Incomplete data!", "Last Name can't be empty", "OK");
+            return;
+        }
+        if (pPosition.SelectedItem == null)
+        {
+            DisplayAlert("Incomplete data!", "Position must be selected", "OK");
+            return;
+        }
+        if (dpDoB.Date == null)
+        {
+            DisplayAlert("Incomplete data!", "Date of birth must be selected", "OK");
+            return;
+        }
+        var lecturer = new LecturerUIModel(Guid.Empty);
+        lecturer.FirstName = eFistName.Text;
+        lecturer.LastName = eLastName.Text;
+        lecturer.Position = ((EnumWithName<LecturerPosition>)pPosition.SelectedItem).Value;
+        lecturer.DateOfBirth = dpDoB.Date.Value;
+        lecturer.SaveChangesToDBModel();
+        DisplayAlert("Lecturer Created!", $"Lecturer {lecturer.FirstName} {lecturer.LastName} was created successfully, his age is {lecturer.Age}", "OK");
+    }
+
+    private void BackClicked(object sender, EventArgs e)
+    {
+        //TODO: navigate back to the previous page
     }
 }
