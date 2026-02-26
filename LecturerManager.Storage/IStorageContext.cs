288471c2a3b87f450c236a86479865932389f9ff
applevovan <yablonskyi.v.reg@gmail.com>
2026-02-26 16:19:15 +0200

    Practice 3 start

---
+++ LecturerManager.Storage/IStorageContext.cs
@@ -0,0 +1,13 @@
+﻿using KMA.ProgrammingInChsarp2026.LecturerManager.DBModels;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace KMA.ProgrammingInCSharp2026.LecturerManager.Services
+{
+    internal interface IStorageContext
+    {
+        IEnumerable<DepartmentDBModel> GetDepartments();
+        IEnumerable<LecturerDBModel> GetLecturersByDepartment(Guid departmentId);
+    }
+}
